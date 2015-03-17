abf0049b44178d3af2834ceef1d08c414af668f4
Ben <ben@notionparallax.co.uk>
2015-03-17 18:28:52 +1100

    wire in icons
    and do some tidying up

--- basic/popup.js
+++ basic/popup.js
@@ -1,90 +1,93 @@
 'use strict';
-var phone_link       = "http://blogs.bvn.com.au/go/phone-list/"
-var projects_link    = "http://blogs.bvn.com.au/go/project-library/"
-var openasset_link   = "http://blogs.bvn.com.au/go/openasset/"
-var archives_link    = ""
-var directories_link = "http://blogs.bvn.com.au/go/browse-file-directories/"
-var sharefile_link   = "https://bvn.sharefile.com"
-var mark_link        = "http://blogs.bvn.com.au/go/bvn-mark/"
-var unpaid_link      = "http://blogs.bvn.com.au/go/unpaid-invoices/"
-var invoicing_link   = "http://planning.bvn.com.au/E4SE"
-var om_link          = "http://blogs.bvn.com.au/go/office-manual/"
-var jobnum_link      = "http://blogs.bvn.com.au/go/job-numbers/"
-var echosign_link    = "http://blogs.bvn.com.au/go/echosign-digital-signing/"
-var wiki_link        = "http://dc2-wiki001/mediawiki/index.php"
-var epicor_link      = "http://planning.bvn.com.au/E4SE"
-var support_link     = "http://servicedesk.bvn.com.au:8080"
+var phone_link       = "http://blogs.bvn.com.au/go/phone-list/";
+var projects_link    = "http://phonelist.bvn.com.au:9000/#/projects/";
+var openasset_link   = "http://blogs.bvn.com.au/go/openasset/";
+var archives_link    = "archives are somewhere back here...";
+var directories_link = "http://blogs.bvn.com.au/go/browse-file-directories/";
+var sharefile_link   = "https://bvn.sharefile.com";
+var mark_link        = "http://blogs.bvn.com.au/go/bvn-mark/";
+var unpaid_link      = "http://blogs.bvn.com.au/go/unpaid-invoices/";
+var invoicing_link   = "http://planning.bvn.com.au/E4SE";
+var om_link          = "http://blogs.bvn.com.au/go/office-manual/";
+var jobnum_link      = "http://blogs.bvn.com.au/go/job-numbers/";
+var echosign_link    = "http://blogs.bvn.com.au/go/echosign-digital-signing/";
+var wiki_link        = "http://dc2-wiki001/mediawiki/index.php";
+var epicor_link      = "http://planning.bvn.com.au/E4SE";
+var support_link     = "http://servicedesk.bvn.com.au:8080";
+var tropos_link      = "http://blogs.bvn.com.au/tropos";
+var bloomfire_link   = "https://bvn.bloomfire.com/";
 
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("Hey guys! The BVN extension seems to have loaded A-OK.");
     $('g').click(function() {
         var fake_a = this.attributes[0].nodeValue;
-        if(fake_a =="open-asset"){console.log(fake_a);}
-		if(fake_a =="project-library"){console.log(fake_a);}
-		if(fake_a =="phonelist"){console.log(fake_a);}
-		if(fake_a =="archives"){console.log(fake_a);}
-		if(fake_a =="directories"){console.log(fake_a);}
-		if(fake_a =="sharefile"){console.log(fake_a);}
-		if(fake_a =="styleguide"){console.log(fake_a);}
-		if(fake_a =="unpaid"){console.log(fake_a);}
-		if(fake_a =="invoicing"){console.log(fake_a);}
-		if(fake_a =="office-manual"){console.log(fake_a);}
-		if(fake_a =="job-numbers"){console.log(fake_a);}
-		if(fake_a =="echosign"){console.log(fake_a);}
-		if(fake_a =="wiki"){console.log(fake_a);}
-		if(fake_a =="epicor"){console.log(fake_a);}
-		if(fake_a =="support"){console.log(fake_a);}
-		if(fake_a =="tropos"){console.log(fake_a);}
-		if(fake_a =="bloomfire"){console.log(fake_a);}
-
-        //var newURL = this.attributes.href.value;
-        //console.log("clicked " + newURL);
-        //chrome.tabs.create({'url': newURL});
+        if(fake_a == "tropos"){
+        	console.log(tropos_link);
+        	chrome.tabs.create({'url': tropos_link});
+        }
+        if(fake_a == "bloomfire"){
+        	console.log(bloomfire_link);
+        	chrome.tabs.create({'url': bloomfire_link});
+        }
+        if(fake_a == "open-asset"){
+        	console.log(openasset_link);
+        	chrome.tabs.create({'url': openasset_link});
+        }
+		if (fake_a == "project-library") {
+		    console.log(projects_link);
+		    chrome.tabs.create({'url': projects_link});
+		}
+		if (fake_a == "phonelist") {
+		    console.log(phone_link);
+		    chrome.tabs.create({'url': phone_link});
+		}
+		if (fake_a == "archives") {
+		    console.log(archives_link);
+		    chrome.tabs.create({'url': archives_link});
+		}
+		if (fake_a == "directories") {
+		    console.log(directories_link);
+		    chrome.tabs.create({'url': directories_link});
+		}
+		if (fake_a == "sharefile") {
+		    console.log(sharefile_link);
+		    chrome.tabs.create({'url': sharefile_link});
+		}
+		if (fake_a == "styleguide") {
+		    console.log(mark_link);
+		    chrome.tabs.create({'url': mark_link});
+		}
+		if (fake_a == "unpaid") {
+		    console.log(unpaid_link);
+		    chrome.tabs.create({'url': unpaid_link});
+		}
+		if (fake_a == "invoicing") {
+		    console.log(invoicing_link);
+		    chrome.tabs.create({'url': invoicing_link});
+		}
+		if (fake_a == "office-manual") {
+		    console.log(om_link);
+		    chrome.tabs.create({'url': om_link});
+		}
+		if (fake_a == "job-numbers") {
+		    console.log(jobnum_link);
+		    chrome.tabs.create({'url': jobnum_link});
+		}
+		if (fake_a == "echosign") {
+		    console.log(echosign_link);
+		    chrome.tabs.create({'url': echosign_link});
+		}
+		if (fake_a == "wiki") {
+		    console.log(wiki_link);
+		    chrome.tabs.create({'url': wiki_link});
+		}
+		if (fake_a == "epicor") {
+		    console.log(epicor_link);
+		    chrome.tabs.create({'url': epicor_link});
+		}
+		if (fake_a == "support") {
+		    console.log(support_link);
+		    chrome.tabs.create({'url': support_link});
+		}
     });
 });
-//chrome.tabs.insertCSS(, "man_eng.css");
-
-
-
-// var epicor_link = "http://blogs.bvn.com.au/go/extranet/"
-// var epicor_link = "http://blogs.bvn.com.au/go/email-manager/"
-// var epicor_link = "http://blogs.bvn.com.au/go/technical-bulletins/"
-// var epicor_link = "http://blogs.bvn.com.au/go/sustainable-design-knowledge-base/"
-// var epicor_link = "http://blogs.bvn.com.au/go/legal-agreements-knowledgebase/"
-// var epicor_link = "http://blogs.bvn.com.au/go/business-development/"
-// var epicor_link = "http://blogs.bvn.com.au/go/"
-// var epicor_link = "http://blogs.bvn.com.au/go/exchange-archives/"
-// var epicor_link = "http://blogs.bvn.com.au/go/library/"
-// var epicor_link = "http://blogs.bvn.com.au/go/computational-design/"
-// var epicor_link = "http://blogs.bvn.com.au/go/bvn-bim/"
-// var epicor_link = "http://blogs.bvn.com.au/go/bvn-bim-content/"
-// var epicor_link = "http://blogs.bvn.com.au/go/sketchup-bvn/"
-// var epicor_link = "http://blogs.bvn.com.au/go/bvn-training/"
-// var epicor_link = "http://blogs.bvn.com.au/go/green-office/"
-// var epicor_link = "https://email.bvn.com.au/owa/"
-// var epicor_link = "http://blogs.bvn.com.au/go/invoicing/"
-// var epicor_link = "http://blogs.bvn.com.au/go/tropos-content-management/"
-// var epicor_link = "http://blogs.bvn.com.au/go/fax-queue-brisbane/"
-// var epicor_link = "http://blogs.bvn.com.au/go/fax-queue-melbourne/"
-// var epicor_link = "http://blogs.bvn.com.au/go/fax-queue-sydney/"
-
-
-
-
-"open-asset"
-"project-library"
-"phonelist"
-"archives"
-"directories"
-"sharefile"
-"styleguide"
-"unpaid"
-"invoicing"
-"office-manual"
-"job-numbers"
-"echosign"
-"wiki"
-"epicor"
-"support"
-"tropos"
-"bloomfire"
